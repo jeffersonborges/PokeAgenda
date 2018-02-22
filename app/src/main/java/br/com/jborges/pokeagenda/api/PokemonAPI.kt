4ff4385b0b4d3d6a9c07ba46a5a73cd4417dc9d1
jeffersonborges <jefferson.gmb@gmail.com>
2018-02-21 23:04:01 -0300

    Initial commit

---
+++ app/src/main/java/br/com/jborges/pokeagenda/api/PokemonAPI.kt
@@ -0,0 +1,14 @@
+package br.com.jborges.pokeagenda.api
+
+import br.com.jborges.pokeagenda.model.Pokemon
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+/**
+ * Created by logonrm on 21/02/2018.
+ */
+interface PokemonAPI{
+    @GET("/api/v2/pokemon/{numero}")
+    fun buscarPokemon(@Path("numero") numeroPokemon: Int): Call<Pokemon>
+}
---
+++ app/src/main/java/br/com/jborges/pokeagenda/model/Pokemon.kt
@@ -0,0 +1,6 @@
+package br.com.jborges.pokeagenda.model
+
+/**
+ * Created by logonrm on 21/02/2018.
+ */
+data class Pokemon(var name: String, var sprites: Sprite)
